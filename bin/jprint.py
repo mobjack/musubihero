d0edc843086269fb08117d00c8548d9ce5941cb3
eric-query <eric@query.ai>
2023-10-06 10:44:19 -0700

    updating jprint

--- bin/jprint.py
+++ bin/jprint.py
@@ -1,8 +1,12 @@
 import sys
 import json
 
+from pygments import highlight
+from pygments.lexers import JsonLexer
+from pygments.formatters import TerminalFormatter
 
-def jprint(print_dict, jdent=2, jkeys=True, eexit=True):
+
+def jprint(print_dict, jdent=2, jkeys=True, color=True, eexit=True):
     '''Prints the dictionary in pretty json and quits'''
 
     if not isinstance(print_dict, dict):
@@ -12,7 +16,11 @@ def jprint(print_dict, jdent=2, jkeys=True, eexit=True):
     if not print_dict:
         raise ValueError('jprint is empty')
 
-    print(json.dumps(print_dict, indent=jdent, sort_keys=jkeys))
+    json_str = json.dumps(print_dict, indent=jdent, sort_keys=jkeys)
+    if color == True:
+        print(highlight(json_str, JsonLexer(), TerminalFormatter()))
+    else:
+        print(json.dumps(print_dict, indent=jdent, sort_keys=jkeys))
 
     if eexit == True:
         sys.exit()
